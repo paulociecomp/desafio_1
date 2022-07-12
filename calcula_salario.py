75502d3dbdc6d057aa083233b8a119c531b3bec2
Paulo Moura <paulo.moura@bhub.ai>
2022-07-12 14:00:48 -0300

    script de exemplo

---
+++ calcula_salario.py
@@ -0,0 +1,9 @@
+hora = input("Quanto vc ganha por hora? ")
+
+horas_trabalhadas = input("Quantas horas voce trabalha por semana? ")
+
+#pega a hora e as horas trabalhadas e calcula o salario líquido
+
+
+# aqui vc mostra o valor líquido calculado
+print("Voce ganha R$ 1000,00 por mês")
